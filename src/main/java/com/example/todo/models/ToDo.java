3aecae70aa578cc88cce46536dff23d377fc8b80
pashtetrus33 <testerrus33@gmail.com>
2023-05-21 00:50:20 +0300

    mongo

--- src/main/java/com/example/todo/models/ToDo.java
+++ src/main/java/com/example/todo/models/ToDo.java
@@ -1,42 +1,38 @@
 package com.example.todo.models;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Entity
+@Document
 @Data
-@NoArgsConstructor
-@Table(name = "ToDo")
 public class ToDo {
+    @NotNull
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
     @NotNull
     @NotBlank
     private String description;
-    @Column(insertable = true, updatable = false)
     private LocalDateTime created;
     private LocalDateTime modified;
     private boolean completed;
 
+    public ToDo() {
+        LocalDateTime date = LocalDateTime.now();
+        this.id = UUID.randomUUID().toString();
+        this.created = date;
+        this.modified = date;
+    }
+
     public ToDo(String description) {
+        this();
         this.description = description;
     }
-
-    @PrePersist
-    void onCreate() {
-        this.setCreated(LocalDateTime.now());
-        this.setModified(LocalDateTime.now());
-    }
-
-    @PreUpdate
-    void onUpdate() {
-        this.setModified(LocalDateTime.now());
-    }
 }
+
+
