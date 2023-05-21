611a1a69715ed8ea9d608ccac55a846af894fb16
pashtetrus33 <testerrus33@gmail.com>
2023-05-21 03:47:43 +0300

    redis

--- src/main/java/com/example/todo/models/ToDo.java
+++ src/main/java/com/example/todo/models/ToDo.java
@@ -4,12 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.redis.core.RedisHash;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
-@Document
 @Data
+@RedisHash
 public class ToDo {
     @NotNull
     @Id
@@ -20,15 +19,13 @@ public class ToDo {
     private LocalDateTime created;
     private LocalDateTime modified;
     private boolean completed;
-
-    public ToDo() {
+    public ToDo(){
         LocalDateTime date = LocalDateTime.now();
         this.id = UUID.randomUUID().toString();
         this.created = date;
         this.modified = date;
     }
-
-    public ToDo(String description) {
+    public ToDo(String description){
         this();
         this.description = description;
     }
